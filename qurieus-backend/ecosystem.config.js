65bf87c90af7c8d4a311bbae3240d19eb7b01076
Sudhir Yadav <er.sudhir.yadav@gmail.com>
2025-07-06 17:17:46 +0000

    Update ecosystem configuration to change Python script path for backend service
    - Modified the script path in ecosystem.config.js to point to the new virtual environment location for the Qurieus backend service.

--- qurieus-backend/ecosystem.config.js
+++ qurieus-backend/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps : [
       {
         name: "qurieus-backend",
-        script: "/home/ubuntu/miniconda3/envs/qurieus-env/bin/python", 
+        script: "/home/ubuntu/qurieus/qurieus-backend/.venv/bin/python", 
         args: " -m uvicorn main:app --host 0.0.0.0 --port 8001", 
         cwd: "/home/ubuntu/qurieus/qurieus-backend",
         instances: 1,
